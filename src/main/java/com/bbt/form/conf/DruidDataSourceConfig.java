34cf8058f999fa39c4d1325e63e3fe89398c1086
Jimi <tang_hu123@163.com>
2024-04-04 15:46:32 +0800

    init

---
+++ src/main/java/com/bbt/form/conf/DruidDataSourceConfig.java
@@ -0,0 +1,15 @@
+package com.bbt.form.conf;
+
+@Configuration
+public class DruidDataSourceConfig {
+
+    /**
+     * 添加 DruidDataSource 组件到容器中，并绑定属性
+     */
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @ConditionalOnProperty(name = "spring.datasource.type", havingValue = "com.alibaba.druid.pool.DruidDataSource")
+    public DataSource druid(){
+        return  new DruidDataSource();
+    }
+}
---
+++ src/test/java/com/bbt/form/FormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbt.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
