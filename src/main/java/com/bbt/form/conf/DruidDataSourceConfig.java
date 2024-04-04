7a3b6c458b545f75dd6cea4ebaba4c3c7e9257b9
Jimi <tang_hu123@163.com>
2024-04-04 16:11:39 +0800

    修复数据源报错

--- src/main/java/com/bbt/form/conf/DruidDataSourceConfig.java
+++ src/main/java/com/bbt/form/conf/DruidDataSourceConfig.java
@@ -1,5 +1,14 @@
 package com.bbt.form.conf;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+import javax.sql.DataSource;
+
 @Configuration
 public class DruidDataSourceConfig {
 
