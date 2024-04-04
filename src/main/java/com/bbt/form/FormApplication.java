aa8f9b5603397801bfe1fc9dea683a41d4164b14
Jimi <tang_hu123@163.com>
2024-04-04 16:30:21 +0800

    尝试引入Slf4j

--- src/main/java/com/bbt/form/FormApplication.java
+++ src/main/java/com/bbt/form/FormApplication.java
@@ -1,13 +1,15 @@
 package com.bbt.form;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class FormApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FormApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(FormApplication.class, args);
+    log.info("应用程序已开启，可以访问！");
+  }
 }
